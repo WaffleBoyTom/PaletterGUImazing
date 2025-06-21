c1ef8b6ac3247cbcfd0e031fbff877cb542ce5e9
WaffleBoyTom <tomhoudini19@gmail.com>
2025-06-21 10:47:47 -0400

    Adds SickDropDown that combines label and combobox

---
+++ PaletterGUImazing/sickdropdown.cpp
@@ -0,0 +1,30 @@
+#include "sickdropdown.h"
+
+#include <QString>
+#include <QtWidgets>
+
+/*
+
+Custom ComboBox that also has a label because
+QT is very atomic
+houdini has spoiled me for too long i guess >?
+
+*/
+
+SickDropDown::SickDropDown(QWidget *parent, QString label)
+{
+    // keep in touch with your parent
+    myCreator = parent;
+    myLayout = new QHBoxLayout(this);
+	myLabel = new QLabel(label, this);
+	myMenu = new QComboBox(this);
+    myLayout->addWidget(myLabel);
+    myLayout->addWidget(myMenu);
+}
+
+void
+SickDropDown::setMenuItem(QString item)
+{
+	myMenu->addItem(item);
+}
+
---
+++ PaletterGUImazing/sickdropdown.h
@@ -0,0 +1,33 @@
+#ifndef SICKDROPDOWN_H
+#define SICKDROPDOWN_H
+
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QComboBox;
+class QWidget;
+class QSlider;
+class QLineEdit;
+class QHBoxLayout;
+class QString;
+QT_END_NAMESPACE
+
+class SickDropDown : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SickDropDown(QWidget *parent, QString label);
+	// FIXME this seems like a shit name because I would expect
+	// this method to take in an index....
+	void setMenuItem(QString item);
+	
+private:
+    QWidget *myCreator;
+    QHBoxLayout *myLayout;
+    QComboBox *myMenu;
+    QLabel *myLabel;
+};
+
+#endif
