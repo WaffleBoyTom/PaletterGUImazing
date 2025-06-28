9632c43baee33a62b4d1db2c60e397a8897f9150
WaffleBoyTom <tomhoudini19@gmail.com>
2025-06-28 19:35:54 -0400

    Utils git add PaletterGUImazing/paletterutils.h

---
+++ PaletterGUImazing/paletterutils.h
@@ -0,0 +1,26 @@
+#ifndef PALETTERUTILS_H
+#define PALETTERUTILS_H
+
+// #include <QtWidgets>
+namespace PaletterUtils
+{
+
+enum class PaletteApplyMode
+{
+    Distance = 0,
+    Luminance,
+    Hue,
+    Saturation       
+};
+
+enum class PaletteGenerationMode
+{
+    MedianCut = 0,
+    K_Means
+};
+
+
+}// end namespace
+
+
+#endif // PALETTERUTILS_H
